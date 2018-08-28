52cff502374407c34c2f68195da7ff96f88bcace
[floenavigation] <[floenavigation@gmail.com]>
2018-08-28 09:53:32 +0200

    Added permissions and Wifi connection code

--- app/src/main/java/com/hfad/slave/MainActivity.java
+++ app/src/main/java/com/hfad/slave/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -30,6 +33,8 @@ import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,6 +48,15 @@ public class MainActivity extends Activity{
     private static Matcher matcherString_AIVDO;
     private static Pattern searchPattern_AIVDM;
     private static Pattern searchPattern_AIVDO;
+    private WifiManager wifiManager;
+    private static final String ssid = "TEST-MOSAIC-B01";
+    private WifiConfiguration wifiConfig;
+    private int networkID;
+    private List<ScanResult> results;
+    private BroadcastReceiver wifiReceiver;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    public static final String ipaddr = "192.168.0.1"; //Server IP address
+    public static final int portId = 2000; //Server port address
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +78,9 @@ public class MainActivity extends Activity{
         Pattern searchPattern_AIVDM = Pattern.compile("AIVDM");
         Pattern searchPattern_AIVDO = Pattern.compile("AIVDO");
 
+    }
+
+    private void enableButtons(){
 
         buttonConnect.setOnClickListener(new OnClickListener() {
 
@@ -74,14 +91,6 @@ public class MainActivity extends Activity{
                         .toString(), Integer.parseInt(editTextPort
                         .getText().toString()), response);
                 myClient.execute();
-                /*
-                try {
-                    socket = new Socket("192.168.0.2", 3000);
-                    readLogic();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }*/
-
             }
         });
 
@@ -92,6 +101,62 @@ public class MainActivity extends Activity{
                 response.setText("");
             }
         });
+
+        Toast.makeText(getApplicationContext(), "Buttons Enabled, enter IP and Port", Toast.LENGTH_LONG).show();
+    }
+
+    private void setWifiReceiver(){
+
+        //Receiver
+        wifiReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                results = wifiManager.getScanResults();
+                //Toast.makeText(getApplicationContext(),"scan results received!!", Toast.LENGTH_LONG).show();
+                //
+                arrayList.clear();
+
+                if(!results.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"scan results received!!", Toast.LENGTH_LONG).show();
+
+                    for (ScanResult scanResult : results) {
+                        arrayList.add(scanResult.SSID + " - " + scanResult.capabilities);
+
+                        /*
+                        //Broadcast to MainActivity
+                        Intent intent_MainAct = new Intent("wifi_updates");
+                        intent_MainAct.putExtra("scanlist", arrayList);
+                        sendBroadcast(intent_MainAct);*/
+
+                        //Toast.makeText(getApplicationContext(), scanResult.SSID, Toast.LENGTH_LONG).show();
+
+                        if(scanResult.SSID.trim().equals(ssid)){
+                            //Toast.makeText(getApplicationContext(), "matched", Toast.LENGTH_LONG).show();
+                            wifiManager.disconnect();
+                            unregisterReceiver(this);
+                            wifiManager.enableNetwork(networkID, true);
+                            boolean success = wifiManager.reconnect();
+                            if(success) {
+                                Toast.makeText(getApplicationContext(), "Connected to AIS", Toast.LENGTH_LONG).show();
+                                Log.d("doInBack","Connected to AIS");
+                                enableButtons();
+                                break;
+                            }else {
+                                wifiManager.reassociate();
+                                Toast.makeText(getApplicationContext(), "Not connected to AIS", Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Empty List!!", Toast.LENGTH_LONG).show();
+                    //registerWifi();
+                    //startScan();
+                }
+            }
+        };
+
     }
 
     private void runtime_permissions() {
@@ -120,13 +185,48 @@ public class MainActivity extends Activity{
                     && grantResults[2] == PackageManager.PERMISSION_GRANTED && grantResults[3] == PackageManager.PERMISSION_GRANTED
                     && grantResults[4] == PackageManager.PERMISSION_GRANTED && grantResults[5] == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(getApplicationContext(),"permGranted", Toast.LENGTH_LONG).show();
-                //enableButton();
+                enableWifiConnection();
 
             }else{
                 //Toast.makeText(getApplicationContext(),"permnotGranted", Toast.LENGTH_LONG).show();
                 runtime_permissions();
             }
         }
+    }
+
+    public void enableWifiConnection(){
+
+        //Enable Wifi
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        try{
+            if (!wifiManager.isWifiEnabled()) {
+                //Toast.makeText(this, "WiFi is disabled ... We need to enable it", Toast.LENGTH_LONG).show();
+                wifiManager.setWifiEnabled(true);
+            }
+        }catch (Exception e){
+            Toast.makeText(this, "wifiManager.isWifiEnabled() returned null", Toast.LENGTH_LONG).show();
+        }
+
+        wifiNetworkConfig();
+        setWifiReceiver();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        startScan();
+
+    }
+
+    private void wifiNetworkConfig() {
+
+        wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = String.format("\"%s\"", ssid);
+        wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        networkID = wifiManager.addNetwork(wifiConfig);
+    }
+
+    private void startScan() {
+
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
     }
 
 
